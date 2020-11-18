2927c727b448a352026d84b39f6502490549e81b
glenn <glenn.ludszuweit@gmail.com>
2020-11-18 09:48:30 +0100

    friends list materialUI

---
+++ src/Components/Global/Friends/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles({
+  table: {
+    minWidth: '100%',
+  },
+  tableHead: {
+    color: '#E6E6E6',
+    backgroundColor: '#800300',
+    fontSize: '16px',
+    fontWeight: 600,
+  },
+});
